9a0a3c4ad8a990380eb2b3e904eeb1e8b6ac1142
goreleaserbot <goreleaser@carlosbecker.com>
2020-07-26 15:31:36 +0200

    Brew formula update for todocli version 1.0.4

--- todocli.rb
+++ todocli.rb
@@ -2,16 +2,16 @@
 class Todocli < Formula
   desc "ToDoCli is a simple cli saving your time by managing your tasks from your terminal."
   homepage ""
-  version "1.0.3"
+  version "1.0.4"
   bottle :unneeded
 
   if OS.mac?
-    url "https://github.com/wstnbrg/todocli/releases/download/1.0.3/todocli_1.0.3_Darwin_x86_64.tar.gz"
-    sha256 "f1612ca78ab780b4215df9bec7f185d817a6810d1cede641d6751362696e9a09"
+    url "https://github.com/wstnbrg/todocli/releases/download/1.0.4/todocli_1.0.4_Darwin_x86_64.tar.gz"
+    sha256 "c90c00ab2813fd9fc4279133e10e2b07ed197885cc2ce4f58d76e7c5f748c627"
   elsif OS.linux?
     if Hardware::CPU.intel?
-      url "https://github.com/wstnbrg/todocli/releases/download/1.0.3/todocli_1.0.3_Linux_x86_64.tar.gz"
-      sha256 "1fefca45efb38ab3d358556621feff5764d1aa5dc2eb576cc3e94a97c6a8fad8"
+      url "https://github.com/wstnbrg/todocli/releases/download/1.0.4/todocli_1.0.4_Linux_x86_64.tar.gz"
+      sha256 "3a70fbf16b912cee205b338aff2e3b6c80e5ce09181a18f16e47ab95318475b2"
     end
   end
 
